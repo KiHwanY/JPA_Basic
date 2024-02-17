6d9cadaa82d6c6b54844e9a7fdbc50bcad91ffc4
KiHwan Yun <drg2524@naver.com>
2024-02-17 14:24:25 +0900

    Entity and table mapping Section

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -3,8 +3,33 @@ package hellojpa;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+/*
+*   [@Entity]
+*   -@Entity가 붙은 클래스는 JPA가 관리. Entity라 한다.
+*   - JPA를 사용해서 테이블과 매핑할 클래스 @Entity 필수
+*
+*   <주의>
+    - 기본 생성자 필수(파라미터가 없는 Public 또는 Protected 생성자)
+    - final 클래스 , enum, interface, inner 클래스 사용 x
+    - 저장할 필드에 final 사용 x
+
+*   [@Entity 속성 정리]
+*   - 속성 : name
+*       -> JPA에서 사용할 엔티티 이름을 지정한다.
+*       -> 기본값 : 클래스 이름을 그대로 사용(예 : Member)
+*       -> 같은 클래스 이름이 없으면 가급적 기본값을 사용한다.
+*
+*   [@Table]
+*   - @Table은 Entity와 매핑할 테이블 지정
+*       - name : 매핑할 테이블 이름 -> 엔티티 이름을 사용
+*       - catalog : 데이터베이스 catalog 매핑
+*       - schema : 데이터베이스 schema 매핑
+*       - uniqueConstraints(DDL) : DDL 생성 시에 유니크 제약 조건 생성
+* */
 
 @Entity //JPA가 관리할 객체
+//@Table(name = "MBR")
 public class Member {
     @Id // 데이터베이스 PK와 매핑
     private Long id;
@@ -34,3 +59,12 @@ public class Member {
         this.name = name;
     }
 }
+
+/*
+*   [Entity Mapping 소개]
+*   - 객체와 테이블 매핑 : @Entity , @Table
+*   - 필드와 컬럼 매핑 : @Column
+*   - 기본 키 매핑 : @Id
+*   - 연관 관계 매핑 : @ManyToOne , @JoinColumn
+*
+* */
