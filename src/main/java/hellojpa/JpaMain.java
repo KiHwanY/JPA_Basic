bb451a3d8f4b125ab9cf95095e872bc5d93e27a1
KiHwan Yun <drg2524@naver.com>
2024-02-26 11:30:03 +0900

    단방향 연관관계

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -3,42 +3,13 @@ package hellojpa;
 import jakarta.persistence.*;
 
 import java.util.List;
-// 실전 예제1 - 요구사항 분석과 기본 매핑 프로젝트 생성 해야함.
+/*
+*
+*
+* */
 public class JpaMain {
 
     public static void main(String[] args) {
-        /*
-        *   [주의]
-        *
-        *   -엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
-        *
-        *   - 엔티티 매니저는 쓰레드간에 공유 x (사용하고 버려야 한다.)
-        *
-        *   - JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
-        * */
-        /*
-        *   [JPQL]
-        *
-        *   가장 단순한 조회 방법
-        *   - EntityManager.find()
-        *   - 객체 그래프 탐색(a.getB().getC())
-        *   ==================================
-        *   1.JPA를 사용하면 엔티티 객체를 중심으로 개발
-        *   2.문제는 검색 쿼리
-        *   3.검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
-        *   4.모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능
-        *   5.애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요
-        *
-        *
-        *   1.JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
-        *   2.SQL과 문법 유사, SELECT,FROM,WHERE,GROUP BY,HAVING,JOIN 지원
-        *   3.JPQL은 엔티티 객체를 대상으로 쿼리
-        *   4.SQL은 데이터베이스 테이블을 대상으로 쿼리
-        *
-        *   1.테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
-        *   2.SQL을 추상화해서 특정 데이터베이스 SQL에 의존X
-        *   3.JPQL을 한마디로 정의하면 객체 지향 SQL
-        * */
 
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
         EntityManager em = emf.createEntityManager();
@@ -46,92 +17,58 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); //  트랜잭션 시작
         try{
-//            Member member = new Member();
-//            member.setId(1L);
-//            member.setName("HelloA");
-//            Member findMember = em.find(Member.class, 1L);// 찾아온다
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
-//            findMember.setName("HelloJPA");
-
-//            [페이징 처리 하는 법]
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(5)
-//                    .setMaxResults(8)
-//                    .getResultList();
-//            for (Member member : result) {
-//                System.out.println("Member.name = "  + member.getName());
-//            }
-
-            //객체를 생성한 상태(비영속)
-//            Member member = new Member();
-//            member.setId(101L);
-//            member.setName("HelloJPA");
-
-//            System.out.println("=== BEFORE ===");
-//            [1차 캐시에 저장됨]
-//            em.persist(member);
-//            System.out.println("=== AFTER ===");
-//            [1차 캐시에서 조회]
-//            Member findMember1 = em.find(Member.class , 101L);
-//            Member findMember2 = em.find(Member.class , 101L);
-//            <1차 캐시로 반복 가능한 읽기(REPEATABLE READ) 등급의 트랜잭션 격리 수준을 데이터베이스가 아닌 애플리케이션 차원에서 제공>
-
-//            System.out.println("result = " + (findMember1 == findMember2));
-
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
-
-
-//            Member member1 = new Member(150L,"A");
-//            Member member2 = new Member(160L,"B");
-
-//            객체를 저장한 상태(영속)
-//            em.persist(member1);
-//            em.persist(member2);
+            // 연관관계 적용 전
+            // 객체를 테이블에 맞추어 모델링 (외래 키 식별자를 직접 다룸)
+//            Team team = new Team();
+//            team.setName("teamA");
+//            em.persist(team);
 //
-//            회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
-//            em.detach(member)
-
-//            엔티티 수정
-//            Member member = em.find(Member.class, 150L);
-//            member.setName("zzzzz");
-//
-
-
-//            엔티티 삭제(객체를 삭제한 상태(삭제))
-//            Member member = em.find(Member.class , 150L);
-//            em.remove(member);
-
 //            Member member = new Member();
-//            member.setUsername("next");
-//            System.out.println("================================");
+//            member.setName("member1");
+//            member.setTeamId(team.getId());
 //            em.persist(member);
-//            System.out.println("member.id = " + member.getId() );
-//            System.out.println("================================");
+//
+//            //(식별자로 다시 조회, 객체 지향적인 방법은 아니다.)
+//            // 조회
+//            Member findMember = em.find(Member.class, member.getId());
+//
+//            Long findTeamId = findMember.getTeamId();
+//
+//            // 연관관계가 없음
+//            Team findTeam = em.find(Team.class, findTeamId);
+            /*
+            *   [객체를 테이블에 맞추어 데이터 중심으로 모델링하면, 협력 관계를 만들 수 없다.]
+            *
+            *   - 테이블은 외래 키로 조인을 사용해서 연관된 테이블을 찾는다.
+            *   - 객체는 참조를 사용해서 연관된 객체를 찾는다.
+            *   - 테이블과 객체 사이에는 이런 큰 간격이 있다.
+            * */
+            // 객체 지향 모델링
+            // (연관관계 저장)
 
-            Member member1 = new Member();
-            member1.setUsername("A");
-            Member member2 = new Member();
-            member2.setUsername("B");
-            Member member3 = new Member();
-            member3.setUsername("C");
+            //저장
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
 
-            System.out.println("================================");
+            Member member = new Member();
+            member.setName("member1");
+            member.setTeam(team);
+            em.persist(member);
 
-            // DB SEQ = 1   | 1
-            // DB SEQ = 51  | 2
-            // DB SEQ = 51  | 3
+            //영속성 컨텍스트 제외 시키고 DB에서 가져오는 쿼리 확인하기
+            em.flush(); // 영속성 컨텍스트에 있는 디비 쿼리 날려서 싱크 맞춤
+            em.clear(); // 영속성 컨텍스트 클리어
 
-            em.persist(member1);
-//            em.persist(member2);
-//            em.persist(member3);
+            // 조회
+            Member findMember = em.find(Member.class, member.getId());
 
-            System.out.println("member1 = " + member1.getId());
-            System.out.println("member2 = " + member2.getId());
-            System.out.println("member3 = " + member3.getId());
-            System.out.println("================================");
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam = " + findTeam.getName());
 
+            // 수정
+            Team newTeam = em.find(Team.class, 100L);
+            findMember.setTeam(newTeam);
 
             tx.commit(); //  트랜잭션 커밋
         } catch (Exception e){
@@ -142,73 +79,4 @@ public class JpaMain {
         emf.close();
     }
 }
-/*
-*   [영속성 컨텍스트]
-*   - JPA를 이해하는데 가장 중요한 용어
-*   - "엔티티를 영구 저장하는 환경"이라는 뜻
-*   - EntityManager.persist(entity);
-*
-*   [엔티티 매니저? 영속성 컨텍스트?]
-*   - 영속성 컨텍스트는 논리적인 개념
-*   - 눈에 보이지 않는다.
-*   - 엔티티 매니저를 통해서 영속성 컨텍스트에 접근
-*
-*   [Entity의 생명 주기]
-*   - 비영속(new/transient) : 영속성 컨텍스트와 전혀 관계가 없는 새로운 상태
-*   - 영속(managed) : 영속성 컨텍스트에 관리되는 상태
-*   - 준영속(detached) : 영속성 컨텍스트에 저장되었다가 분리된 상태
-*   - 삭제(removed) : 삭제된 상태
-*
-*   [영속성 컨텍스트의 이점]
-*   - 1차 캐시
-*   - 동일성(identity) 보장
-*   - 트랜잭션을 지원하는 쓰기 지연(transactional write-behind)
-*   - 변경 감지(Dirty Checking)
-*   - 지연 로딩(Lazy Loading)
-*
-*   [플러시]
-*   - 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영
-*
-*   [플러시 발생]
-*   - 변경 감지
-*   - 수정된 엔티티 쓰기 지연 SQL 저장소에 등록
-*   - 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 전송(등록,수정,삭제 쿼리)
-*
-*   [영속성 컨텍스트를 플러시하는 방법]
-*   - em.flush() : 직접 호출
-*   - 트랜잭션 커밋 : 플러시 자동 호출
-*   - JPQL 쿼리 실행 - 플러시 자동 호출
-*
-*   [JPQL 쿼리 실행 시 플러시가 자동으로 호출되는 이유]
-*
-    * em.persist(memberA);
-     em.persist(memberB);
-     em.persist(memberC);
-     //중간에 JPQL 실행
-    query = em.createQuery("select m from Member m", Member.class);
-     List<Member> members= query.getResultList();
 
-*   [플러시 모드 옵션]
-*   em.setFlushMode(FlushModeType.COMMIT)
-*
-*   - FlushModeType.AUTO - > 커밋이나 쿼리를 실행할 때 플러시(기본값) -> 이걸 사용해라
-*   - FlushModeType.COMMIT : 커밋할 때만 플러시
-*
-*   [ 플러시 정리 ]
-*   - 영속성 컨텍스트를 비우지 않음
-*   - 영속성 컨텍스트의 변경 내용을 데이터베이스에 동기화
-*   - 트랜잭션이라는 작업 단위가 중요 -> 커밋 직전에만 동기화 하면 됨
-*
-*   [ 준영속 상태 ]
-*   - 영속 -> 준영속
-*   - 영속 상태의 엔티티가 영속성 컨텍스트에서 분리(detached)
-*   - 영속성 컨텍스트가 제공하는 기능을 사용 못함
-*
-*   [ 준영속 상태로 만드는 방법]
-*   - em.detach(entity)
-*       -> 특정 엔티티만 준영속 상태로 전환
-*   - em.clear()
-*       -> 영속성 컨텍스트를 완전히 초기화
-*   - em.close()
-*       -> 영속성 컨텍스트를 종료
-* */
---
+++ src/main/java/hellojpa/JpaMainEntityMappingSection.java
@@ -0,0 +1,216 @@
+package hellojpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+// 실전 예제1 - 요구사항 분석과 기본 매핑 프로젝트 생성 해야함.
+//public class JpaMainEntityMappingSection {
+//
+//    public static void main(String[] args) {
+//        /*
+//        *   [주의]
+//        *
+//        *   -엔티티 매니저 팩토리는 하나만 생성해서 애플리케이션 전체에서 공유
+//        *
+//        *   - 엔티티 매니저는 쓰레드간에 공유 x (사용하고 버려야 한다.)
+//        *
+//        *   - JPA의 모든 데이터 변경은 트랜잭션 안에서 실행
+//        * */
+//        /*
+//        *   [JPQL]
+//        *
+//        *   가장 단순한 조회 방법
+//        *   - EntityManager.find()
+//        *   - 객체 그래프 탐색(a.getB().getC())
+//        *   ==================================
+//        *   1.JPA를 사용하면 엔티티 객체를 중심으로 개발
+//        *   2.문제는 검색 쿼리
+//        *   3.검색을 할 때도 테이블이 아닌 엔티티 객체를 대상으로 검색
+//        *   4.모든 DB 데이터를 객체로 변환해서 검색하는 것은 불가능
+//        *   5.애플리케이션이 필요한 데이터만 DB에서 불러오려면 결국 검색 조건이 포함된 SQL이 필요
+//        *
+//        *
+//        *   1.JPA는 SQL을 추상화한 JPQL이라는 객체 지향 쿼리 언어 제공
+//        *   2.SQL과 문법 유사, SELECT,FROM,WHERE,GROUP BY,HAVING,JOIN 지원
+//        *   3.JPQL은 엔티티 객체를 대상으로 쿼리
+//        *   4.SQL은 데이터베이스 테이블을 대상으로 쿼리
+//        *
+//        *   1.테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
+//        *   2.SQL을 추상화해서 특정 데이터베이스 SQL에 의존X
+//        *   3.JPQL을 한마디로 정의하면 객체 지향 SQL
+//        * */
+//
+//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+//        EntityManager em = emf.createEntityManager();
+//
+//        EntityTransaction tx = em.getTransaction();
+//        tx.begin(); //  트랜잭션 시작
+//        try{
+////            Member member = new Member();
+////            member.setId(1L);
+////            member.setName("HelloA");
+////            Member findMember = em.find(Member.class, 1L);// 찾아온다
+////            System.out.println("findMember.id = " + findMember.getId());
+////            System.out.println("findMember.name = " + findMember.getName());
+////            findMember.setName("HelloJPA");
+//
+////            [페이징 처리 하는 법]
+////            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+////                    .setFirstResult(5)
+////                    .setMaxResults(8)
+////                    .getResultList();
+////            for (Member member : result) {
+////                System.out.println("Member.name = "  + member.getName());
+////            }
+//
+//            //객체를 생성한 상태(비영속)
+////            Member member = new Member();
+////            member.setId(101L);
+////            member.setName("HelloJPA");
+//
+////            System.out.println("=== BEFORE ===");
+////            [1차 캐시에 저장됨]
+////            em.persist(member);
+////            System.out.println("=== AFTER ===");
+////            [1차 캐시에서 조회]
+////            Member findMember1 = em.find(Member.class , 101L);
+////            Member findMember2 = em.find(Member.class , 101L);
+////            <1차 캐시로 반복 가능한 읽기(REPEATABLE READ) 등급의 트랜잭션 격리 수준을 데이터베이스가 아닌 애플리케이션 차원에서 제공>
+//
+////            System.out.println("result = " + (findMember1 == findMember2));
+//
+////            System.out.println("findMember.id = " + findMember.getId());
+////            System.out.println("findMember.name = " + findMember.getName());
+//
+//
+////            Member member1 = new Member(150L,"A");
+////            Member member2 = new Member(160L,"B");
+//
+////            객체를 저장한 상태(영속)
+////            em.persist(member1);
+////            em.persist(member2);
+////
+////            회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
+////            em.detach(member)
+//
+////            엔티티 수정
+////            Member member = em.find(Member.class, 150L);
+////            member.setName("zzzzz");
+////
+//
+//
+////            엔티티 삭제(객체를 삭제한 상태(삭제))
+////            Member member = em.find(Member.class , 150L);
+////            em.remove(member);
+//
+////            Member member = new Member();
+////            member.setUsername("next");
+////            System.out.println("================================");
+////            em.persist(member);
+////            System.out.println("member.id = " + member.getId() );
+////            System.out.println("================================");
+//
+//            Member member1 = new Member();
+//            member1.setUsername("A");
+//            Member member2 = new Member();
+//            member2.setUsername("B");
+//            Member member3 = new Member();
+//            member3.setUsername("C");
+//
+//            System.out.println("================================");
+//
+//            // DB SEQ = 1   | 1
+//            // DB SEQ = 51  | 2
+//            // DB SEQ = 51  | 3
+//
+//            em.persist(member1);
+////            em.persist(member2);
+////            em.persist(member3);
+//
+//            System.out.println("member1 = " + member1.getId());
+//            System.out.println("member2 = " + member2.getId());
+//            System.out.println("member3 = " + member3.getId());
+//            System.out.println("================================");
+//
+//
+//            tx.commit(); //  트랜잭션 커밋
+//        } catch (Exception e){
+//            tx.rollback();
+//        }finally {
+//            em.close();
+//        }
+//        emf.close();
+//    }
+//}
+/*
+*   [영속성 컨텍스트]
+*   - JPA를 이해하는데 가장 중요한 용어
+*   - "엔티티를 영구 저장하는 환경"이라는 뜻
+*   - EntityManager.persist(entity);
+*
+*   [엔티티 매니저? 영속성 컨텍스트?]
+*   - 영속성 컨텍스트는 논리적인 개념
+*   - 눈에 보이지 않는다.
+*   - 엔티티 매니저를 통해서 영속성 컨텍스트에 접근
+*
+*   [Entity의 생명 주기]
+*   - 비영속(new/transient) : 영속성 컨텍스트와 전혀 관계가 없는 새로운 상태
+*   - 영속(managed) : 영속성 컨텍스트에 관리되는 상태
+*   - 준영속(detached) : 영속성 컨텍스트에 저장되었다가 분리된 상태
+*   - 삭제(removed) : 삭제된 상태
+*
+*   [영속성 컨텍스트의 이점]
+*   - 1차 캐시
+*   - 동일성(identity) 보장
+*   - 트랜잭션을 지원하는 쓰기 지연(transactional write-behind)
+*   - 변경 감지(Dirty Checking)
+*   - 지연 로딩(Lazy Loading)
+*
+*   [플러시]
+*   - 영속성 컨텍스트의 변경 내용을 데이터베이스에 반영
+*
+*   [플러시 발생]
+*   - 변경 감지
+*   - 수정된 엔티티 쓰기 지연 SQL 저장소에 등록
+*   - 쓰기 지연 SQL 저장소의 쿼리를 데이터베이스에 전송(등록,수정,삭제 쿼리)
+*
+*   [영속성 컨텍스트를 플러시하는 방법]
+*   - em.flush() : 직접 호출
+*   - 트랜잭션 커밋 : 플러시 자동 호출
+*   - JPQL 쿼리 실행 - 플러시 자동 호출
+*
+*   [JPQL 쿼리 실행 시 플러시가 자동으로 호출되는 이유]
+*
+    * em.persist(memberA);
+     em.persist(memberB);
+     em.persist(memberC);
+     //중간에 JPQL 실행
+    query = em.createQuery("select m from Member m", Member.class);
+     List<Member> members= query.getResultList();
+
+*   [플러시 모드 옵션]
+*   em.setFlushMode(FlushModeType.COMMIT)
+*
+*   - FlushModeType.AUTO - > 커밋이나 쿼리를 실행할 때 플러시(기본값) -> 이걸 사용해라
+*   - FlushModeType.COMMIT : 커밋할 때만 플러시
+*
+*   [ 플러시 정리 ]
+*   - 영속성 컨텍스트를 비우지 않음
+*   - 영속성 컨텍스트의 변경 내용을 데이터베이스에 동기화
+*   - 트랜잭션이라는 작업 단위가 중요 -> 커밋 직전에만 동기화 하면 됨
+*
+*   [ 준영속 상태 ]
+*   - 영속 -> 준영속
+*   - 영속 상태의 엔티티가 영속성 컨텍스트에서 분리(detached)
+*   - 영속성 컨텍스트가 제공하는 기능을 사용 못함
+*
+*   [ 준영속 상태로 만드는 방법]
+*   - em.detach(entity)
+*       -> 특정 엔티티만 준영속 상태로 전환
+*   - em.clear()
+*       -> 영속성 컨텍스트를 완전히 초기화
+*   - em.close()
+*       -> 영속성 컨텍스트를 종료
+* */
