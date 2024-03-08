23220ef384d53a0ea21cb9a88ce36bab42226804
KiHwan Yun <drg2524@naver.com>
2024-03-08 15:20:52 +0900

    다대일 [N:1] Section commit

---
+++ src/main/java/hellojpa/MemberMappingSection.java
@@ -0,0 +1,98 @@
+package hellojpa;
+
+
+import jakarta.persistence.*;
+
+
+/*
+ *   [ 목표 ]
+ *   - 객체와 테이블 연관 관계의 차이를 이해
+ *   - 객체의 참조와 테이블의 외래 키를 매핑
+ *
+ *   - 용어 이해
+ *       -> 방향(Direction) : 단방향, 양방향
+ *       -> 다중성(Multiplicity) : 다대일(N:1) , 일대다(1:N), 일대일(1:1), 다대다(N:M) 이해
+ *       -> 연관관계의 주인(Owner) : 객체 양방향 연관관계는 관리 주인이 필요 (중요한 용어다.)
+ * */
+/*
+*   [ 예제 시나리오 ]
+*   - 회원과 팀이 있다.
+*   - 회원은 하나의 팀에만 소속될 수 있다.
+*   - 회원과 팀은 다대일 관계다.
+*
+*   [ 연관관계의 주인(Owner) ]
+*   -양방향 매핑 규칙
+*       - 객체의 두 관계중 하나를 연관관계의 주인으로 지정
+*       - 연관관계의 주인만이 외래 키를 관리(등록,수정)
+*       - 주인이 아닌쪽은 읽기만 가능
+*       - 주인은 mappedBy 속성 사용 x
+*       - 주인이 아니면 mappedBy 속성으로 주인 지정
+*
+*   [ 누구를 주인으로? ]
+*   - 외래 키가 있는 곳을 주인으로 정해라.
+*   - 여기서 Member.team이 연관관계의 주인이다.
+* */
+@Entity
+public class MemberMappingSection {
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String name;
+
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne // 멤버 입장에서는 N 이고 팀은 1이기 때문에
+    @JoinColumn(name = "TEAM_ID") // join 시킬 컬럼 지정
+    private Team team; // 단방향 연관관계 , 연관관계 주인이다.
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
+    //자바의 getter set 관례때문에 set은 set 관련 로직 있을 때만 넣어준다.
+    // 로직이 추가 되면 네임 수정하고 사용한다.
+//    public void changeTeam(Team team) {
+//        this.team = team;
+//        team.getMembers().add(this);
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "Member{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", team=" + team +
+//                '}';
+//    }
+    //    public Long getTeamId() {
+//        return teamId;
+//    }
+//
+//    public void setTeamId(Long teamId) {
+//        this.teamId = teamId;
+//    }
+}
--- src/main/java/hellojpa/Team.java
+++ src/main/java/hellojpa/Team.java
@@ -4,15 +4,6 @@ import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
-/*
-*   [ 객체의 양방향 관계 ]
-*   - 객체의 양방향 관계는 사실 양방향 관계가 아니라 서로 다른 단방향 관계 2개다.
-*   - 객체를 양방향으로 참조하려면 단방향 연관관계를 2개 만들어야 한다.
-*
-*   [ 테이블의 양방향 연관관계 ]
-*   - 테이블은 외래 키 하나로 두 테이블의 연관관계를 관리
-*   - MEMBER.TEAM_ID 외래 키 하나로 양방향 연관관계 가짐(양쪽으로 조인할 수 있다.)
-* */
 
 @Entity
 public class Team { // 주인이 아닌 쪽은 읽기만 가능하다.
@@ -23,23 +14,9 @@ public class Team { // 주인이 아닌 쪽은 읽기만 가능하다.
 
     private String name;
 
-    // add 할 때 nullPoint 방지를 위한 ArrayList 초기화
-    @OneToMany(mappedBy = "team") // 일대다 매핑해서 주인의 어떤 변수와 매핑되어야 하는지?
+    @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
-    /*
-    *   [ 객체와 테이블이 관계를 맺는 차이 ]
-    *   - 객체 연관관계 = 2개
-    *       -  회원 -> 팀 연관관계 1개(단방향)
-    *       -  팀 -> 회원 연관관계 1개(단방향)
-    *   - 테이블 연관관계 = 1개
-    *       - 회원 <-> 팀의 연관관계 1개(양방향)
-    * */
 
-    // 연관관계 편의 메서드가 양쪽에 다 있으면 문제를 일으킬 수 있기 때문에 한 곳에만 추가해 주자.
-//    public void addMember(Member member){
-//        member.setTeam(this);
-//        members.add(member);
-//    }
     public Long getId() {
         return id;
     }
@@ -56,21 +33,6 @@ public class Team { // 주인이 아닌 쪽은 읽기만 가능하다.
         this.name = name;
     }
 
-    public List<Member> getMembers() {
-        return members;
-    }
 
-    public void setMembers(List<Member> members) {
-        this.members = members;
-    }
-//    양방향 연관관계 주의 - 양방향 매핑 시에 무한 루프를 조심하자.
-//    특히 toString() , lombok, JSON 생성 라이브러리 - 스택 오브 플로우 확률 높다.
-//    @Override
-//    public String toString() {
-//        return "Team{" +
-//                "id=" + id +
-//                ", name='" + name + '\'' +
-//                ", members=" + members +
-//                '}';
-//    }
+
 }
