8988c4b08cd5ef4ccb2bf37c47efa5e8633711ab
KiHwan Yun <drg2524@naver.com>
2024-03-13 22:08:42 +0900

    member 수정

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -38,13 +38,5 @@ public class Member {
     public void setTeam(Team team) {
         this.team = team;
     }
-
-    @Override
-    public String toString() {
-        return "Member{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", team=" + team +
-                '}';
-    }
+    
 }
