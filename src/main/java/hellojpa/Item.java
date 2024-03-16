9968be4ac2ef0f947cb734001baf2ff9dc93f113
KiHwan Yun <drg2524@naver.com>
2024-03-16 18:37:36 +0900

    Ingeritance SINGLE_TABLE Strategy Section

--- src/main/java/hellojpa/Item.java
+++ src/main/java/hellojpa/Item.java
@@ -3,9 +3,13 @@ package hellojpa;
 import jakarta.persistence.*;
 
 @Entity
-@Inheritance(strategy = InheritanceType.JOINED) // Join 전략
-@DiscriminatorColumn(name = "DIS_TYPE") // Default name = DTYPE
-public class Item {
+//@Inheritance(strategy = InheritanceType.JOINED) // Join 전략
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // SINGLE_TABLE 전략
+//@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS) // TABLE_PER_CLASS 전략 -> 권장하지 않는다.
+@DiscriminatorColumn(name = "DIS_TYPE") // Default name = DTYPE ,
+// SINGLE_TABLE 전략에서는 DiscriminatorColumn을 설정 안해도 자동으로 생성된다.
+// TABLE_PER_CLASS 전략에서는 DiscriminatorColumn을 테이블 자체가 다르기 때문에 사용해도 의미가 없다.
+public abstract class Item { // 추상 클래스화
     @Id
     @GeneratedValue
     private Long id;
