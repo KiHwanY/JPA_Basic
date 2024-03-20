9acae6a62c07d726ff66ed039f3cdb1e7dd4e826
KiHwan Yun <drg2524@naver.com>
2024-03-20 19:36:54 +0900

    CASCADE AND REMOVE SECTION

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,30 +16,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); //  트랜잭션 시작
         try{
-            Team teamA = new Team();
-            teamA.setName("teamA");
-            em.persist(teamA);
-
-            Team teamB = new Team();
-            teamB.setName("teamA");
-            em.persist(teamB);
-
-            Member member1 = new Member();
-            member1.setName("member1");
-            member1.setTeam(teamA);
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setName("member2");
-            member2.setTeam(teamB);
-            em.persist(member2);
-
-            em.flush();
-            em.clear();
-
-            Member m = em.find(Member.class , member1.getId());
-
-            List<Member> members = em.createQuery("select m from Member m",Member.class).getResultList();
+//            Team teamA = new Team();
+//            teamA.setName("teamA");
+//            em.persist(teamA);
+//
+//            Team teamB = new Team();
+//            teamB.setName("teamA");
+//            em.persist(teamB);
+//
+//            Member member1 = new Member();
+//            member1.setName("member1");
+//            member1.setTeam(teamA);
+//            em.persist(member1);
+//
+//            Member member2 = new Member();
+//            member2.setName("member2");
+//            member2.setTeam(teamB);
+//            em.persist(member2);
+//
+//            em.flush();
+//            em.clear();
+//
+//            Member m = em.find(Member.class , member1.getId());
+//
+//            List<Member> members = em.createQuery("select m from Member m",Member.class).getResultList();
 
 //            System.out.println("m = " + m.getTeam().getClass());
 //
@@ -47,6 +47,26 @@ public class JpaMain {
 //            System.out.println("teamName = " + m.getTeam().getName());
 //            m.getTeam().getName(); // getName을 호출하는 순간 초기화
 //            System.out.println("========================");
+            Child child1 = new Child();
+            Child child2 = new Child();
+
+            Parent parent = new Parent();
+
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
+//            em.persist(child1);
+//            em.persist(child2);
+            em.flush();
+            em.clear();
+
+            Parent findParent = em.find(Parent.class, parent.getName());
+            findParent.getChildList().remove(0L);
+            
+
+
+
 
             tx.commit(); //  트랜잭션 커밋
         } catch (Exception e){
