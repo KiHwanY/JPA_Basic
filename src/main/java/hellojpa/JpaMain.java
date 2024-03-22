6536932cb4b28c4d71642f55857ad6433969d1f8
KiHwan Yun <drg2524@naver.com>
2024-03-22 17:56:41 +0900

    Embeddable Section

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -16,13 +16,30 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin(); //  트랜잭션 시작
         try{
-
+            Address address = new Address("city" , "street","100");
             Member member = new Member();
             member.setName("hello");
-            member.setAddress(new Address("city" , "street","100"));
+            member.setAddress(address);
             member.setPeriod(new Period());
-
             em.persist(member);
+
+            //불변 객체 설정
+            Address address1 = new Address(address.getStreet(),  address.getZipcode(), "NewCity");
+            member.setAddress(address1);
+
+            //값 타입은 불변 객체로 만들어야 한다.
+
+            //값 타입에서 첫번째 인덱스 값을 바꾸려면 복사를 해서 사용해야 한다.
+//            Address copyAddress = new Address(address.getCity(),address.getCity(), address.getZipcode());
+//
+//            Member member2 = new Member();
+//            member2.setName("hello2");
+//            member2.setAddress(copyAddress);
+//            member2.setPeriod(new Period());
+//            em.persist(member2);
+//
+//            //절대 못잡는 버그. 업데이트 쿼리가 두개 나간다.
+//            member.getAddress().setCity("newCity");
 
 
             tx.commit(); //  트랜잭션 커밋
